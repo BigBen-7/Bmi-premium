5daae4e1881d12e1bbcc4db7f14722c80e345b41
BigBen-7 <103938678+BigBen-7@users.noreply.github.com>
2023-02-06 00:20:59 +0100

    Update BMI.js

--- BMI.js
+++ BMI.js
@@ -1,6 +1,7 @@
 const btnEl = document.getElementById("btn");
 const bmiInputEl = document.getElementById("bmi-result");
 const weightConditionEl = document.getElementById("weight-condition");
+const weightTipsEl = document.getElementById("weight-tips");
 
 // MODAL DECLERATION
 const modal = document.querySelector(".modal");
