cf41ff42da716673c58d6e2299e4c73a45fa986a
BigBen-7 <103938678+BigBen-7@users.noreply.github.com>
2023-02-06 00:19:04 +0100

    Update BMI.js

--- BMI.js
+++ BMI.js
@@ -45,12 +45,18 @@ function calculateBMI() {
 
   if (bmiValue < 18.5) {
     weightConditionEl.innerText = "Severe Thinness";
+     weightTipsEl.innerHTML =
+      "<h4 class='tips'>Risk associated with being underweight</h4>\n <p class= 'tips-para'>Being underweight has its own associated risks, listed below:</p>\n <ul class='tips-list'><li>Malnutrition, vitamin deficiencies, anemia (lowered ability to carry blood vessels)</li> <li>Osteoporosis, a disease that causes bone weakness, increasing the risk of breaking a bone </li> <li>Type II diabetes</li> <li>Coronary heart disease</li> <li>A decrease in immune function</li> <li>Growth and development issues, particularly in children and teenagers</li> <li>Potential complications as a result of surgery</li> <li>Generally, an increased risk of mortality compared to those with a healthy BMI</li></ul>\n <p class= 'tips-para'>As can be seen from the list above, there are numerous negative, in some cases fatal, outcomes that may result from being overweight. Generally, a person should try to maintain a BMI between 18-25 kg/m2. If you fall in the overweight category, we will send monthly weight loss and nutritional tips to help you lose weight directly to your email. </p>";
   } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
     weightConditionEl.innerText = "Normal Weight";
   } else if (bmiValue >= 25 && bmiValue <= 29.9) {
     weightConditionEl.innerText = "OverWeight";
+     weightTipsEl.innerHTML =
+      "<h4 class='tips'>Risk associated with being overweight</h4>\n <p class= 'tips-para'>Being overweight increases the risk of a number of serious diseases and health conditions. Below is a list of said risks, according to the Centers for Disease Control and Prevention (CDC):</p>\n <ul class='tips-list'><li>High blood pressure</li> <li>Higher levels of LDL </li> <li>Type II diabetes</li> <li>Coronary heart disease</li> <li>Stroke</li> <li>Gallbladder disease</li> <li>Low quality of life</li> <li>Mental illnesses such as clinical depression, anxiety, and others</li></ul>\n <p class= 'tips-para'>As can be seen from the list above, there are numerous negative, in some cases fatal, outcomes that may result from being overweight. Generally, a person should try to maintain a BMI between 18-25 kg/m2. If you fall in the overweight category, we will send monthly weight loss and nutritional tips to help you lose weight directly to your email. </p>";
   } else if (bmiValue >= 30) {
     weightConditionEl.innerText = "Obesity";
+     weightTipsEl.innerHTML =
+      "<h4 class='tips'>Risk associated with being overweight</h4>\n <p class= 'tips-para'>Being overweight increases the risk of a number of serious diseases and health conditions. Below is a list of said risks, according to the Centers for Disease Control and Prevention (CDC):</p>\n <ul class='tips-list'><li>High blood pressure</li> <li>Higher levels of LDL </li> <li>Type II diabetes</li> <li>Coronary heart disease</li> <li>Stroke</li> <li>Gallbladder disease</li> <li>Low quality of life</li> <li>Mental illnesses such as clinical depression, anxiety, and others</li></ul>\n <p class= 'tips-para'>As can be seen from the list above, there are numerous negative, in some cases fatal, outcomes that may result from being overweight. Generally, a person should try to maintain a BMI between 18-25 kg/m2. If you fall in the overweight category, we will send monthly weight loss and nutritional tips to help you lose weight directly to your email. </p>";
   }
 }
 
